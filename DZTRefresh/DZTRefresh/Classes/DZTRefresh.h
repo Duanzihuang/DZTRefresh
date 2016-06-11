ce0284f5f41b69b0e09599ffdec49153e2538a10
manageritcast <manageritcast@126.com>
2016-06-11 17:13:10 +0800

    02.完成了1.0
    02.完成了1.0

---
+++ DZTRefresh/DZTRefresh/Classes/DZTRefresh.h
@@ -0,0 +1,15 @@
+//
+//  DZTRefresh.h
+//  DZTRefresh
+//
+//  Created by apple on 16/6/11.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DZTRefresh : UIControl
+
+- (void)beginRefresh;
+
+@end
---
+++ DZTRefresh/DZTRefresh/Classes/DZTRefresh.m
@@ -0,0 +1,24 @@
+//
+//  DZTRefresh.m
+//  DZTRefresh
+//
+//  Created by apple on 16/6/11.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import "DZTRefresh.h"
+
+@implementation DZTRefresh
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (void)beginRefresh{
+    NSLog(@"假装在更新!!!");
+}
+
+@end
